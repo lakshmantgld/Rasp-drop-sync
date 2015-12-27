a756b5e5538d9dbc91d0fb83da0c116db4a293a7
lakshman <diwaakartg@gmail.com>
2015-12-27 21:36:29 +0900

    py file which syncs with dropbox

--- droprasp.py
+++ droprasp.py
@@ -1,25 +1,35 @@
 #!/usr/bin/python
+# 
+#
+#
+#
+#
+
+
+from __future__ import division
 import os
 import dropbox
 import yaml
 
-with open("./config.yml", "r") as fd:
+with open("/Users/lakshman/entaag/lddroprasp/config.yml", "r") as fd:
     info = yaml.load(fd)
+try:
+    print("starting point after access token retrieval from yaml file")
 
-print("starting point after access token retrieval from yaml file")
+    dbx = dropbox.Dropbox(info["app_token"])
 
-dbx = dropbox.Dropbox(info["app_token"])
+    list_of_directories_handler = dbx.files_list_folder("")
 
-list_of_directories_handler = dbx.files_list_folder("")
+    list_of_directories = [];
 
-list_of_directories = [];
-
-for entry in list_of_directories_handler.entries:
-    list_of_directories.append(entry.name)
+    for entry in list_of_directories_handler.entries:
+        list_of_directories.append(entry.name)
+except:
+    print " error in access token and listing files from dropbox"
 
 # print(list_of_directories)
 
-os.chdir("../raspsync")
+os.chdir("/Users/lakshman/entaag/raspsync")
 
 for directory in list_of_directories:
     print(directory)
@@ -32,19 +42,41 @@ for directory in list_of_directories:
     dropbox_files = []
     addition_list = []
     deletion_list = []
-    for media in dbx.files_list_folder("/"+ directory).entries:
-        dropbox_files.append(media.name)
-    print("dropbox_files   ")
-    print(dropbox_files)
-    deletion_list = list(set(local_files) - set(dropbox_files))
-    addition_list = list(set(dropbox_files) - set(local_files))
-    if addition_list:
-        print("files to be downloaded:")
-        print(addition_list)
-        for media in addition_list:
-            print(dbx.files_download_to_file("/Users/lakshman/entaag/raspsync/" + directory + "/" + media, "/"+ directory + "/"+ media))
-    if deletion_list:
-        print("files to be deleted:")
-        print(deletion_list)
-        for media in deletion_list:
-            os.remove("/Users/lakshman/entaag/raspsync/" + directory + "/" + media)
+    try:
+        for media in dbx.files_list_folder("/"+ directory).entries:
+            dropbox_files.append(media.name)
+        print("dropbox_files   ")
+        print(dropbox_files)
+        deletion_list = list(set(local_files) - set(dropbox_files))
+        addition_list = list(set(dropbox_files) - set(local_files))
+        if addition_list:
+            print("files to be downloaded:")
+            print(addition_list)
+            for media in addition_list:
+                filename = open("/Users/lakshman/entaag/raspsync/" + directory + "/" + media, "wb")
+                try:
+                    metadata, req  = dbx.files_download("/"+ directory + "/"+ media)
+                    print(metadata.size)
+                    total = 0
+                    while total < metadata.size:
+                        filename.write(req.raw.read(4096))
+                        total += 4096
+                        print("percentage completed:")
+                        print (int(((total/int(metadata.size)) * 100)))
+            # print("the request object is ")
+            # print(req.content)
+            # req.read(4096);
+                    if deletion_list:
+                        print("files to be deleted:")
+                        print(deletion_list)
+                        for media in deletion_list:
+                            os.remove("/Users/lakshman/entaag/raspsync/" + directory + "/" + media)
+                except:
+                    os.remove("/Users/lakshman/entaag/raspsync/" + directory + "/" + media)
+                    print "download error"
+                else:
+                    print "small try completed"
+    except dropbox.exceptions.ApiError:
+        print "error"
+    else:
+        print "Big try completed"
